2634d4a038ce8bef7050b66b7800e6b1aabaa08f
zwilk314 <zwilkerson314@gmail.com>
2018-11-18 11:51:28 -0500

    Added VERY BASIC handling of NA values and nominal to integer encoding; initial attempt at creating and evaluating a regressor object using gradient boosting

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn import model_selection
+import numpy as np
 
 def readData():
     inputsCol = ['Id', 'MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street',
@@ -62,6 +66,31 @@ def poolQCConversion(x):    # there is no poor for pool
     else:
         return x
 
+"""
+Generic numerical encoder for string values ('a' = 0, 'b' = 1, so on - nothing special)
+Original code, based on https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.LabelEncoder.html
+"""
+def encodeNominalData(inputDF, inputCols):
+    reps = len(inputCols)
+    for i in range(reps):
+        print(i)
+        labelEncoder = preprocessing.LabelEncoder()
+        labelEncoder.fit(inputDF.iloc[:,i])
+        inputDF.iloc[:,i] = labelEncoder.transform(inputDF.iloc[:,i])
+        
+def manageNAValues(inputDF, inputCols):
+    
+    #First, deal with columns where "NA" actually means something
+    significantNAFields = ["Alley","BsmtQual","BsmtCond","BsmtExposure","BsmtFinType1",
+                           "FireplaceQu","GarageType","GarageFinish","GarageQual","GarageCond",
+                           "PoolQC","Fence","MiscFeature"]
+    for columnName in significantNAFields:
+        inputDF.loc[:,columnName].map(lambda val: "NoValue" if val == "NA" else val)
+    
+    #Then, fill in remaining "NA" values (currently with mode of other column entries)
+    for column in inputCols:
+        inputDF.loc[:,column] = inputDF.loc[:,column].fillna(inputDF.loc[:,column].mode()[0])
+
 def preprocess(targetDF, sourceDF):
     exConversionCols = ["ExterQual", "ExterCond", "BsmtCond", "KitchenQual", "FireplaceQu", "GarageQual", "GarageCond"]
     targetDF.loc[:, exConversionCols] = targetDF.loc[:,exConversionCols].applymap(lambda x: nominalValueConversion(x))  # Ex, Gd, TA, Fa, Po to be numerical value 1.0, 0.75, 0.5, 0.25 0.0
@@ -72,7 +101,13 @@ def preprocess(targetDF, sourceDF):
 
 def main():
     trainDF, inputsCol, outputCol = readData()
-    preprocess(trainDF, trainDF)
-    print(trainDF.loc[0:10, ["ExterQual", "ExterCond", "BsmtCond", "KitchenQual", "FireplaceQu", "GarageQual", "GarageCond",
-                     "PoolQC"]])
+    #preprocess(trainDF, trainDF)
+    manageNAValues(trainDF, inputsCol)
+    encodeNominalData(trainDF, inputsCol)
+    alg = GradientBoostingRegressor()
+    cvScores = model_selection.cross_val_score(alg, trainDF.loc[:, inputsCol], trainDF.loc[:, outputCol], cv=10, scoring='mean_squared_error')
+    print(np.mean(cvScores))
+    #print(trainDF.loc[0:10, ["ExterQual", "ExterCond", "BsmtCond", "KitchenQual", "FireplaceQu", "GarageQual", "GarageCond",
+    #                 "PoolQC"]])
+    print(trainDF.loc[0:10,:])
 main()
