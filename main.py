ec4bae6bfc31b721a5d14b7fdc6b775bb3da677c
zwilk314 <zwilkerson314@gmail.com>
2018-11-30 11:34:57 -0500

    Working with single feature removal, experiments included in new Excel spreadsheet

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import reading
 import visualization
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn import model_selection
+import copy
+import pandas as pd
 
 
 """
@@ -46,18 +48,34 @@ def main():
     preprocessing.preprocess(trainDF, trainDF, inputsCol)
     alg = GradientBoostingRegressor(random_state = 1)   # accuracy does not change everytime it is run with set random_state
     cvScores = model_selection.cross_val_score(alg, trainDF.loc[:, inputsCol], trainDF.loc[:, outputCol], cv=10, scoring='r2')
-    print("Highest Accuracy with all featuers =", np.mean(cvScores))
+    print("Highest Accuracy with all features =", np.mean(cvScores))
     #visualization(trainDF,inputsCol,outputCol)
     
-    inputsColTemp = inputsCol
+    #Already done, testing what happens to accuracy removing one feature at a time
+    """
+    inputsColTemp = copy.deepcopy(inputsCol)
+    temp = {}
+    temp["Nothing removed"] = np.mean(cvScores)
     while len(inputsColTemp) != 0:
         featureRemoved = inputsColTemp.pop()
         inputsCol.remove(featureRemoved)
         alg = GradientBoostingRegressor(random_state = 1)
         cvScores = model_selection.cross_val_score(alg, trainDF.loc[:, inputsCol], trainDF.loc[:, outputCol], cv=10, scoring='r2')
+        temp[featureRemoved] = np.mean(cvScores)
         print("Accuracy when removing " + featureRemoved + " =", np.mean(cvScores))
         inputsCol.append(featureRemoved)
+    export = pd.Series(temp)
+    export.to_csv("/Users/zmwilk/Desktop/test.csv")
+    """
+    
+    itemsToRemove = set(["HalfBath", "LandSlope","BldgType","YearBuilt","LowQualFinSF","Utilities","1stFlrSF",
+                         "GarageCond","ScreenProch","OpenPorchSF","EnclosedPorch"])
+    post_featureRemoval = filter(lambda x: x not in itemsToRemove, inputsCol)
+    alg = GradientBoostingRegressor(random_state = 1)
+    cvScores = model_selection.cross_val_score(alg, trainDF.loc[:, post_featureRemoval], trainDF.loc[:, outputCol], cv=10, scoring='r2')
+    print("After removing all detrimental features =", np.mean(cvScores))
+    #This results in a lower value... does this mean some of these are related, or simply need preprocessing
+    #   (e.g., year built should probably become age)?
 
-    visualization.visualize(trainDF,inputsCol,outputCol)
->>>>>>> refs/remotes/DMFinalProject/master
+    #visualization.visualize(trainDF,inputsCol,outputCol)
 main()
